cd98d7194edc84fc0ccafa2966847df96f499d21
Christine Tacha and Gracie Lundell <tacha.christine@gmail.com>
2016-01-11 12:56:47 -0800

    Add ul

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,21 @@
 $(document).ready(function() {
-};
+  $("h1").click(function() {
+    alert("This is a header.");
+  });
+
+  $("p").click(function() {
+    alert("This is a paragraph.");
+  });
+
+  $("img").click(function() {
+    alert("This is an image.");
+  });
+
+  $("h2").click(function() {
+    alert("This is another header")
+  });
+
+  $("ul").click(function() {
+    alert("There are many more creatures to be discovered!")
+  });
+});
