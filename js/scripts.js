17dfc311e224f115b3c8389d6a885b68dbdd02dd
Christine Tacha and Gracie Lundell <tacha.christine@gmail.com>
2016-01-11 13:39:46 -0800

    Toggle image

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,6 @@
 $(document).ready(function() {
-  $("h1").click(function() {
-    alert("This is a header.");
-  });
-
   $("p").click(function() {
-    alert("This is a paragraph.");
-  });
-
-  $("img").click(function() {
-    alert("This is an image.");
-  });
-
-  $("h2").click(function() {
-    alert("This is another header")
-  });
-
-  $("ul").click(function() {
-    alert("There are many more creatures to be discovered!")
+    $(".image-shown").toggle();
+    $(".image-hidden").toggle();
   });
 });
